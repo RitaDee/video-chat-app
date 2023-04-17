42c1c9162f1df1b165986c26a9404d9c91c35ad2
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-04-17 20:01:45 +0100

    Add peerConnection

---
+++ main.js
@@ -0,0 +1,43 @@
+let localStream;
+let remoteStream;
+let peerConnection;
+
+const servers = {
+    iceServers: [
+        {
+            urls:['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
+        }
+    ],
+}
+let init = async () => {
+    localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+    document.getElementById('user-1').srcObject = localStream;
+
+    createOffer();
+};
+
+let createOffer = async () => {
+    peerConnection = new RTCPeerConnection(servers)
+
+    remoteStream = new MediaStream()
+    document.getElementById('user-2').srcObject = remoteStream;
+
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, localStream)
+    })
+
+    peerConnection.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+            remoteStream.addTrack()
+        })
+    }
+
+    peerConnection.onicecandidate
+
+    let offer = await peerConnection.createOffer()
+        await peerConnection.setLocalDescription (offer)
+    
+     console.log('Offer', offer)
+}
+
+init()
